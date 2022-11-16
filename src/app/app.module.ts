abaeb561613c87bd0b08e583b16c356c4db99b1a
Pablo Jiménez de Yarza <pablo.jimenez@bosonit.com>
2022-11-16 17:53:23 +0100

    commit final 16 nov

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatButtonModule} from '@angular/material/button';
+
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
 import { AppComponent } from './app.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { Process1Component } from './process1/process1.component';
+import { Process2Component } from './process2/process2.component';
+import { Process3Component } from './process3/process3.component';
 
 @NgModule({
   imports: [
@@ -17,10 +24,16 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
     ComponentsModule,
     RouterModule,
     AppRoutingModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatButtonModule
   ],
   declarations: [
     AppComponent,
     AdminLayoutComponent,
+    Process1Component,
+    Process2Component,
+    Process3Component,
 
   ],
   providers: [],
